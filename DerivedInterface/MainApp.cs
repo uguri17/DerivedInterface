f837f49196b30e3351d81b60ab1666447096c0d3
RND-HW3 <RND-HW3@RND-HW3>
2025-03-26 13:52:02 +0900

    초기 커밋.

---
+++ DerivedInterface/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DerivedInterface
+{
+	interface ILogger // based interface
+	{
+		void WriteLog(string message);
+	}
+
+	interface IFormattableLogger : ILogger // derived interface
+	{
+        void WriteLog(string format, params Object[] args);
+    }
+
+	class ConsoleLogger2 : IFormattableLogger // 상속받은 class는 public만 와야한다..
+    {
+		public void WriteLog(string message)
+		{
+			Console.WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+		}
+
+        public void WriteLog(string format, params Object[] args)
+        {
+			string message = String.Format(format, args);
+			Console.WriteLine($"{DateTime.Now.ToLocalTime()}, {message}");
+        }
+    }
+
+	class MainApp
+	{
+		static void Main(string[] args)
+		{
+            IFormattableLogger logger = new ConsoleLogger2();
+			logger.WriteLog("The world is not flat.");
+            logger.WriteLog($"{1} + {1} = {2}");
+        }
+	}
+}
